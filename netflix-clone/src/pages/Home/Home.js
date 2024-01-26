b665a743aef2642027fdd2723ad8f76f4b06ffa7
ttddww <woldeteketel@gmail.com>
2024-01-26 00:55:35 -0500

    row

--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -2,12 +2,13 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import Banner from "../../components/Banner/Banner";
-
+import RowLIst from "../../components/Rows/RowLIst/RowLIst";
 const Home = () => {
   return (
     <>
       <Header />
       <Banner />
+      <RowLIst />
       <Footer />
     </>
   );
